5762c5b0f99fcfc16a18c9bfda3dde7447911a68
David Furnes <david@dfurnes.com>
2019-06-07 13:13:10 -0400

    Wait until DOMContentLoaded to insert badge.

--- src/register.js
+++ src/register.js
@@ -1,5 +1,14 @@
 import badge from './index';
 
+// Simple 'DOMContentLoaded' helper, via http://youmightnotneedjquery.com/#ready:
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', () => fn());
+  }
+}
+
 // For browser builds (or impatient npm users) we'll provide
 // this entry-point which autoruns the package w/ defaults.
-badge();
+ready(badge);

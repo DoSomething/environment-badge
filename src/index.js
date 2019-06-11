902033fa07fb174f8090a098393fda4cc181c2d2
David Furnes <david@dfurnes.com>
2019-06-11 14:27:29 -0400

    Prevent triggering the function.name polyfill. <https://git.io/fjg1Y>

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 const DEFAULT_ENVIRONMENTS = [
   {
-    name: 'local',
+    displayName: 'local',
     host: /(^localhost(:[0-9]+)?$|\.test$)/,
   },
   {
-    name: 'dev',
+    displayName: 'dev',
     host: /^([a-z0-9-]*-)?dev\./,
     backgroundColor: '#fcd116',
   },
   {
-    name: 'qa',
+    displayName: 'qa',
     host: /^([a-z0-9-]*-)?qa\./,
     backgroundColor: '#23b7fb',
     foregroundColor: '#fff',
   },
   {
-    name: 'preview',
+    displayName: 'preview',
     host: /^([a-z0-9-]*-)?preview\./,
     backgroundColor: '#22BC66',
     foregroundColor: '#fff',
@@ -23,21 +23,25 @@ const DEFAULT_ENVIRONMENTS = [
 ]
 
 export default (environments = DEFAULT_ENVIRONMENTS) => {
-  const environment = environments.find(
-    environment => environment.host.test(window.location.host)
-  );
+  let environment = null;
+  for (let i = 0; i < environments.length; i++) {
+    if (environments[i].host.test(window.location.host)) {
+      environment = environments[i]
+      break;
+    }
+  }
 
   // Create the badge and add it to the page:
   if (environment) {
-    const { name, backgroundColor = '#ddd', foregroundColor = '#000' } = environment;
+    const { displayName, backgroundColor = '#ddd', foregroundColor = '#000' } = environment;
     const environmentBadge = document.createElement('div');
-    environmentBadge.innerHTML = `
-        <div style="display: block; position: absolute; top: -50px; left: -50px; width: 100px; height: 100px; background: ${backgroundColor}; color: ${foregroundColor}; transform: rotate(-45deg); z-index: 9999;">
-          <span style="display: block; position: relative; top: 85%; font-size: 12px; text-align: center; font-weight: bold; text-transform: uppercase; transform: translateY(-50%); cursor: default;">
-            ${name}
-          </span>
-        </div>
-      `;
+    environmentBadge.innerHTML = '\
+        <div style="display: block; position: absolute; top: -50px; left: -50px; width: 100px; height: 100px; background: ' + backgroundColor + '}; color: ' + foregroundColor + '}; transform: rotate(-45deg); z-index: 9999;">\
+          <span style="display: block; position: relative; top: 85%; font-size: 12px; text-align: center; font-weight: bold; text-transform: uppercase; transform: translateY(-50%); cursor: default;">\
+            ' + displayName + '\
+          </span>\
+        </div>\
+      ';
 
     // Apply badge to Forge container if it exists, or the body:
     const container = document.querySelector('.chrome > .wrapper') || document.body;

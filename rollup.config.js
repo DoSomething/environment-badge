72d4a19d0d8955168e4c37d5a5a7429a946589aa
David Furnes <david@dfurnes.com>
2019-06-11 14:09:47 -0400

    Update dependencies, including Babel & Rollup.

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,14 @@
 import babel from 'rollup-plugin-babel';
+import { uglify } from "rollup-plugin-uglify";
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default ({
   input: 'src/register.js',
-  plugins: [babel()],
+  plugins: [
+    babel({runtimeHelpers: true}),
+    nodeResolve(),
+    uglify(),
+  ],
   output: [{
     file: 'dist/bundle.js',
     format: 'iife'
